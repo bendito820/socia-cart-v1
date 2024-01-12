818140560e4fe9fe90cae5b5283b441ad2346448
Antonio Costa <bendito820@gmail.com>
2024-01-12 14:41:13 +0100

    Correcting bug at the url and '/'...

--- mappers/listings.js
+++ mappers/listings.js
@@ -1,6 +1,6 @@
 const mapper = (listing) => {
   // const baseUrl = "http://192.168.0.21:8000/assets/";
-  const baseUrl = "https://socia-cart-api-v1.onrender.com/assets";
+  const baseUrl = "https://socia-cart-api-v1.onrender.com/assets/";
   const mapImage = (image) => ({
     url: `${baseUrl}${image.fileName}.jpg`,
     thumbnailUrl: `${baseUrl}${image.fileName}.jpg`,
