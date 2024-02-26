58976f7b658ad260e47d2c40792fe79f1a3879a0
Antonio Costa <bendito820@gmail.com>
2024-02-26 18:43:19 +0100

    update listings Id

--- store/listings.js
+++ store/listings.js
@@ -37,7 +37,7 @@ const listings = [
     },
   },
   {
-    id: 6,
+    id: 7,
     title: "Meia Caixa - Perna de Frango",
     price: 8743.47,
     description: "Perna de Frango - Meia Caixa (5kg)",
@@ -63,7 +63,7 @@ const listings = [
 
   // Hereee!
   {
-    id: 8,
+    id: 9,
     title: "Linguiça - 4U",
     price: 145555.88,
     description: "Linguiça -  4 unidades (1kg)",
@@ -75,7 +75,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 10,
     title: "Linguiça - 3U",
     price: 10973.2,
     description: "Linguiça -  3 unidades /(1kg)",
@@ -87,7 +87,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 11,
     title: "Arroz Patriota (5kg)",
     price: 10973.2,
     description: "Arroz Patriota - Caixa de 12 unidades (5kg)",
@@ -99,7 +99,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 12,
     title: "Massa Esparguete Primaveira",
     price: 6448.19,
     description:
@@ -112,7 +112,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 13,
     title: "Massa Penne Macarrão Rigate Primaveira",
     price: 6757.31,
     description:
@@ -125,7 +125,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 14,
     title: "Peixe Carapau - 15kg",
     price: 28807.59,
     description: "Peixe Carapau - 15kg - Poupas 442,50kz",
@@ -137,7 +137,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 15,
     title: "Peixe Pescado - 10kg",
     price: 16643.33,
     description: "Peixe Pescado - 10kg - Poupas 256,67kz",
@@ -149,7 +149,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 16,
     title: "Ovos - 4U",
     price: 16404.52,
     description: "Ovos - 4 ubidades - 10kg - Poupas 169,67kz",
@@ -161,7 +161,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 17,
     title: "Coxa USA KOCH Foods",
     price: 15379.28,
     description: "Coxa USA KOCH Foods - 10kg - Poupas 96,72kz",
@@ -173,7 +173,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 18,
     title: "Coxa Seara Brazil",
     price: 15379.28,
     description: "Coxa Seara Brazil - 10kg - Poupas 106,72kz",
@@ -185,7 +185,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 19,
     title: "Asa de Peru",
     price: 33985.59,
     description: "Asa de Peru - 10kg - Poupas 216,72kz",
@@ -197,7 +197,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 20,
     title: "Galinha Rija Dura",
     price: 15351.71,
     description: "Galinha Rija Dura - 5 Unidades - Poupas 216,72kz",
@@ -209,7 +209,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 21,
     title: "Frango Perdix",
     price: 11431.37,
     description: "Frango Perdix - 3 Unidades - Poupas 118,39kz",
@@ -221,7 +221,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 22,
     title: "Leite Meio Gordo Mimosa 1L",
     price: 10000,
     description:
@@ -234,7 +234,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 23,
     title: "Leite Meio Gordo Mimosa 1L 4UN",
     price: 6701.29,
     description:
@@ -247,7 +247,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 24,
     title: "Leite Meio Gordo Mimosa 1L 3UN",
     price: 5051.29,
     description:
@@ -260,7 +260,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 25,
     title: "5 KG - Batata Rena Nacional ",
     price: 4536.98,
     description: "Batata Rena Nacional - Caixa de  5 KG - Poupas 25,39kz",
@@ -272,7 +272,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 26,
     title: "5 KG - Cebola Nacional",
     price: 7959,
     description: "Cebola Nacional- Caixa de  5 KG - Poupas 25,39kz",
@@ -284,7 +284,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 27,
     title: "5 KG - Batata Doce Nacional",
     price: 6504,
     description: "Batata Doce Nacional- Caixa de  5 KG - Poupas 33,39kz",
@@ -296,7 +296,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 28,
     title: "Banana Pão",
     price: 1400,
     description: "Banana Pão Nacional- Caixa de  1 KG - Poupas 33,39kz",
@@ -308,7 +308,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 29,
     title: "Abacate Nacional",
     price: 1900,
     description: "Abacate Nacional -  2,50 KG - Poupas 33,39kz",
@@ -320,7 +320,7 @@ const listings = [
     },
   },
   {
-    id: 8,
+    id: 30,
     title: "GIMBOA",
     price: 800,
     description: "GIMBOA - 1.00 - Poupas 33,39kz",
